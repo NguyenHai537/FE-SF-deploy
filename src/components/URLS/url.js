72f179be546dab2b12afe0a186cbd4524a6ffd0b
nguyenhai <nguyenhai190497@gmail.com>
2023-04-26 08:52:11 +0700

    first commit

---
+++ src/components/URLS/url.js
@@ -0,0 +1,6 @@
+export const PRODUCT_URL = "https://springboot-modul-6.herokuapp.com/api/product"
+export const CART_URL = "https://springboot-modul-6.herokuapp.com/api/cart"
+export const ACCOUNT_URL = "https://springboot-modul-6.herokuapp.com/api/account"
+export const ORDER_URL = "https://springboot-modul-6.herokuapp.com/api/order"
+export const STORE_URL = "https://springboot-modul-6.herokuapp.com/api/store"
+
